cc899c1d471e5dbb539331174abbcfe298484a44
mamamiajazz <datplayinman@gmail.com>
2026-04-14 18:34:50 +0000

    23

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 console.log("Script started")
 
 // TODO: Test working with objects here
+let person = {
+    name: "Paul",
+     age: "80",
+     eyeColor: "yellow";
+}
+
+console.log(person.name);
+
+person.name = "Allen";
+console.log(person.name);
+
+person.birthday = "Jan 1";
+console.loh(person);
 
 
-
-function processForm() {
+function processForm(event) {
     console.log("click");
     // TODO: prevent this function from reloading the page when the form is submitted
+    event.preventDefault();
 
     // TODO: Create a newUser object that has all the user's info from the form
+    let newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        favColor: document.getElementById("favcolor").value,
+    }
 
     // TODO: Call the addUser function and pass the newUser object as a parameter
 }
